1456e1289ac6b0c421dfc4d7188d4777c0796894
ctran0105 <catanh1520@gmail.com>
2023-08-05 21:47:41 -0400

    Initialize React project with basic component.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// This contains main app components
+import Header from "./components/Header";
 
 function App() {
+  const title = "Blog Post";
+  const body = "This is my post";
+  const comments = [
+    { id: 1, text: "Comment 1" },
+    { id: 2, text: "Comment 2" },
+    { id: 3, text: "Comment 3" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>{title}</h1>
+        <p>{body}</p>
+
+        <div className="comments"></div>
+        <h2>Comments ({comments.length})</h2>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
