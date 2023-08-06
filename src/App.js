11d50c6284103fae258bca0ed12d8b5471ee2109
ctran0105 <catanh1520@gmail.com>
2023-08-06 10:35:08 -0400

    Add header styling

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@
 import Header from "./components/Header";
 
 function App() {
-  const title = "Blog Post";
-  const body = "This is my post";
-  const comments = [
-    { id: 1, text: "Comment 1" },
-    { id: 2, text: "Comment 2" },
-    { id: 3, text: "Comment 3" },
-  ];
-
   return (
     <>
       <Header />
       <div className="container">
-        <h1>{title}</h1>
-        <p>{body}</p>
-
-        <div className="comments"></div>
-        <h2>Comments ({comments.length})</h2>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
+        <h1>My App</h1>
       </div>
     </>
   );
