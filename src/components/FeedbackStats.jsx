87ff22cf3840b3fe4c356a9195247cab3ffafc23
ctran0105 <catanh1520@gmail.com>
2023-08-09 00:20:17 -0400

    Continue building FeedbackStats component and reactivity

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,9 +1,25 @@
-function FeedbackStats() {
+import PropTypes from "prop-types";
+
+function FeedbackStats({ feedback }) {
+  // Calculate feedback rating
+  let average =
+    feedback.reduce((acc, curr) => {
+      return acc + curr.rating;
+    }, 0) / feedback.length;
+
+  // Make avarage 1 decimal place and replace .0 at the end with blank space
+  average = average.toFixed(1).replace(/[.,]0$/, "");
+
   return (
-    <div>
-      Stats
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
-  )
+  );
 }
 
-export default FeedbackStats
+FeedbackStats.PropTypes = {
+  feedback: PropTypes.array.isRequired,
+};
+
+export default FeedbackStats;
