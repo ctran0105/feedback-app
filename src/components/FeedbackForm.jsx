4fff017bdaaa4971f03c0d20d0e6659b7c733b4d
ctran0105 <catanh1520@gmail.com>
2023-08-09 00:40:53 -0400

    Set new state for FeedbackForm

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Card from "./shared/Card";
 
 function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
         {/* @todo - rating select component */}
         <div className="input-group">
-          <input type="text" placeholder="Write a review" />
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
           <button type="submit">Send</button>
         </div>
       </form>
