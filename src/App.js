27145b2f38e93fc3deec58421e44a2d3ec9dc3f1
ctran0105 <catanh1520@gmail.com>
2023-08-08 23:58:13 -0400

    Add delete button and build deleteFeedback function

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import { useState } from "react";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are your sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
