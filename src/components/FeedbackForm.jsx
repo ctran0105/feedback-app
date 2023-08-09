90c3fbe39de4c334ba4c5fc75fbc65f0bb9fc9ac
ctran0105 <catanh1520@gmail.com>
2023-08-09 00:34:56 -0400

    Add a Card component to FeedbackForm. Initialize an input with submit button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,18 @@
+import Card from "./shared/Card";
+
 function FeedbackForm() {
   return (
-    <div>
-      Form
-    </div>
-  )
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* @todo - rating select component */}
+        <div className="input-group">
+          <input type="text" placeholder="Write a review" />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
