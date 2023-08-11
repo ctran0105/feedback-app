2fd8dd8985e2ad37c7ca43198fa5b480f09d60e6
ctran0105 <catanh1520@gmail.com>
2023-08-10 21:45:32 -0400

    Build handleSubmit function for feedback form and add it to global componentin App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // This contains main app components
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -7,9 +8,12 @@ import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import RatingSelect from "./components/RatingSelect";
 
-
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are your sure you want to delete?")) {
@@ -21,7 +25,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
