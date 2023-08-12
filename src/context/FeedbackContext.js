b5def1ff46b6630eb28e7dbf2b9550bc506f7863
ctran0105 <catanh1520@gmail.com>
2023-08-12 15:49:27 -0400

    Add more data into context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,8 +7,28 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: "This item is from context",
+      text: "This feedback item 1",
+      rating: 6,
+    },
+    {
+      id: 2,
+      text: "This feedback item 2",
       rating: 10,
+    },
+    {
+      id: 3,
+      text: "This feedback item 3",
+      rating: 7,
+    },
+    {
+      id: 4,
+      text: "This feedback item 4",
+      rating: 4,
+    },
+    {
+      id: 5,
+      text: "This feedback item 5",
+      rating: 5,
     },
   ]);
 
